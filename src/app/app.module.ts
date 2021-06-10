80027b5f354f67c67d19d8ea1cb168100a1a6547
vlmaslennikov <v.maslennikov.q@gmail.com>
2021-06-10 10:27:07 +0300

    rename some folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { QuillComponent } from './quill/quill.component';
-import { DetailsComponent } from './details/details.component';
+import { QuillComponent } from './quill-component/quill.component';
+import { DetailsComponent } from './details-page/details.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { RoutingModule } from './router/routing.module';
+import { RoutingModule } from './router-module/routing.module';
 
 @NgModule({
   declarations: [
--- src/app/router-module/routing.module.ts
+++ src/app/router-module/routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { DetailsComponent } from '../details/details.component';
+import { DetailsComponent } from '../details-page/details.component';
 import { MainPageComponent } from '../main-page/main-page.component';
 
 const appRoutes: Routes =[
